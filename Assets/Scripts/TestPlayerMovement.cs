eb17e89dbdd42952e548cd88c547cae09b497122
Justin Ha <jch583@nyu.edu>
2016-11-13 01:59:04 -0500

    Test player movement works, now to work on spawner

---
+++ Assets/Scripts/TestPlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayerMovement : MonoBehaviour {
+
+	public int moveSpeed; 
+
+	// Update is called once per frame
+	void Update () {
+
+		float inputX = Input.GetAxis ("Horizontal"); 
+		float inputZ = Input.GetAxis ("Vertical"); 
+
+		transform.Translate (inputX * moveSpeed * Time.deltaTime, 0f, 0f);
+		transform.Translate (0f, 0f, inputZ * moveSpeed * Time.deltaTime); 
+	}
+}
