47bcba03544d44928df55a90a2dd44f99b2f6afa
Justin Ha <jch583@nyu.edu>
2016-11-13 20:16:13 -0500

    Got spawner to spawn every 5 seconds

---
+++ Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySpawner : MonoBehaviour {
+
+	//public Transform playerPos; 
+	public GameObject Enemy; 
+
+	public int spawnBuffer; 
+
+	private float spawnTime; 
+	private bool canSpawn = false; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		spawnTime += Time.deltaTime; 
+		Debug.Log (spawnTime);
+		if (spawnTime > spawnBuffer) {
+			canSpawn = true; 
+			//Instantiate (Enemy, transform.position, transform.rotation);
+		}
+
+		if (canSpawn == true) {
+			Instantiate (Enemy, transform.position, transform.rotation);
+			canSpawn = false; 
+		}
+	}
+}
