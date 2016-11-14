f18cd7641929410e0b73273ffe473af7a0da6aef
Justin Ha <jch583@nyu.edu>
2016-11-13 23:10:05 -0500

    Started enemyMovement script

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMovement : MonoBehaviour {
+
+	public Transform playerPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
