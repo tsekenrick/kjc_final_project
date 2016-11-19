5a016a3bc3216f8373837818fea6c5b35f8cc1ce
Justin Ha <jch583@nyu.edu>
2016-11-19 18:29:25 -0500

    latest update

--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -8,21 +8,33 @@ public class enemySpawner : MonoBehaviour {
 
 	public int spawnBuffer; 
 
+	private float randombuffer; 
+	private float initialSpawn; 
+
 	private float spawnTime; 
+
+	private bool spawnCycle = false; 
 
 	// Use this for initialization
 	void Start () {
-	
+		initialSpawn = Random.Range (0, 7); 
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		if (spawnTime > spawnBuffer + initialSpawn) {
+			Instantiate (Enemy, transform.position, transform.rotation);
+			spawnTime = 0;
+			spawnCycle = true;
+		}
+
 		// Make the spawnTime go up by 1 every second
 		spawnTime += Time.deltaTime; 
 
 		// If the spawnTime > spawnBuffer, spawn the enemy and reset the spawnTime 
-		if (spawnTime > spawnBuffer) {
+		if (spawnTime > spawnBuffer + randombuffer && spawnCycle == true) {
+			randombuffer = Random.Range (0, 5);
 			Instantiate (Enemy, transform.position, transform.rotation);
 			spawnTime = 0;
 		}
